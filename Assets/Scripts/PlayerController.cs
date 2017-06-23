2eaaa52cba36917c176fe5a0743fc2fde0331eac
auusi9 <ausi_dalmau@hotmail.com>
2017-06-23 12:02:36 +0200

    4 directions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,10 @@ public class PlayerController : MonoBehaviour {
     public GameObject horizontal_shoot_point;
     public int velocity = 4;
     private Vector3 shoot_point;
-    bool di = false;
-	// Use this for initialization
-	void Start ()
+    bool di_x = false;
+    bool di_z = false;
+    // Use this for initialization
+    void Start ()
     {
         body = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
@@ -27,13 +28,10 @@ public class PlayerController : MonoBehaviour {
         float v = Input.GetAxis("Vertical");
         Vector3 vel = new Vector3(h, 0,v) * velocity;
         Debug.Log("horizontal: " + h + "Vertical: " + v);
+        di_x = (h > 0) ? false : true;
+        di_z = (v > 0) ? false : true;
 
-        if (h > 0)
-             di = false;
-        else if (h < 0)
-            di = true;
-
-        GetComponent<SpriteRenderer>().flipX = di;
+        GetComponent<SpriteRenderer>().flipX = di_x;
         body.velocity = vel;
     }
     void Update()
@@ -42,7 +40,7 @@ public class PlayerController : MonoBehaviour {
         {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
-            Vector3 vel = new Vector3(Mathf.Abs(h), 0, v);
+            Vector3 vel = new Vector3(Mathf.Abs(h), 0, Mathf.Abs(v));
 
             if (vel.sqrMagnitude > 0.2 )
                 ShootBullet(vel);
@@ -53,10 +51,21 @@ public class PlayerController : MonoBehaviour {
 
     void ShootBullet(Vector3 dir)
     {
-        int i = di ? -1 : 1;
+        int x = di_x ? -1 : 1;
+        int z = di_z ? -1 : 1;
+
         Vector3 shoot_tmp = shoot_point;
-        shoot_tmp.x = shoot_point.x * i;
-        dir.x *= i;
+        if (dir.x < dir.z)
+        {
+            shoot_tmp.z = shoot_tmp.x;
+            shoot_tmp.x = 0;
+        }
+            
+        shoot_tmp.x *= x;
+        shoot_tmp.z *= z;
+        
+        dir.x *= x;
+        dir.z *= z;
         GameObject bull_tmp = Instantiate(bullet, transform.position + shoot_tmp, Quaternion.identity);
         bull_tmp.GetComponent<Rigidbody>().AddForce(dir  * bullet_force);
         Destroy(bull_tmp, 10.0f);
