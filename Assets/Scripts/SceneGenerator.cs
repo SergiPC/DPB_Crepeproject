ddc93e037fa6ada0867a31c9559df73d9c743d08
auusi9 <ausi_dalmau@hotmail.com>
2017-06-30 13:51:09 +0200

    Level generation started, completly random right now

---
+++ Assets/Scripts/SceneGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneGenerator : MonoBehaviour {
+
+    public GameObject beggining;
+    public GameObject ending;
+    public GameObject[] scene_parts;
+
+    public int number_of_sections = 10;
+
+    float x_size = 0f;
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject go = Instantiate(beggining, transform);
+
+        x_size = go.GetComponent<Collider>().bounds.size.x;
+        Debug.Log(go.GetComponent<Collider>().bounds.size);
+        Vector3 pos = transform.position;
+        for(int i = 0; i< number_of_sections; i++)
+        {
+            pos.x += x_size;
+            int x = Random.Range(0, scene_parts.Length);
+            go = Instantiate(scene_parts[x], pos, transform.rotation);
+            x_size = go.GetComponent<Collider>().bounds.size.x;
+        }
+        pos.x += x_size;
+
+       Instantiate(ending, pos, transform.rotation);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
