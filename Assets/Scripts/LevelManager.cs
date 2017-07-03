b88f58122969de3b59a57560eb256f4bb95a70a3
auusi9 <ausi_dalmau@hotmail.com>
2017-07-03 13:08:06 +0200

    Bullet sprite and added animation

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum IN_GAME_STATUS
+{
+    STARTING,
+    IN_GAME,
+    PAUSE,
+    WINNING,
+    LOSING
+}
+
+
 public class LevelManager : MonoBehaviour {
 
     public int player_num;
     GameObject[] players_in_game;
-	// Use this for initialization
-	void Start ()
+    IN_GAME_STATUS current_status;
+    // Use this for initialization
+    void Start ()
     {
+        current_status = IN_GAME_STATUS.STARTING;
         players_in_game = GameObject.FindGameObjectsWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update ()
+    {
+
+    }
+
+    public void ChangeStatus(IN_GAME_STATUS new_status)
     {
 
     }
